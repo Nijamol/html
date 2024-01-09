bfce7ed9aef8f80f32597ce29d6ed883de0bb012
Nijamol <146943067+Nijamol@users.noreply.github.com>
2024-01-09 12:41:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function buttonValidation form()
+{
+var name=document.myform.mobile.value;
+var mobileRegex=/^\d{10}
+if(!mobile Regex=test(name)){
+
+alert ("Enter valid mobile no");
+return false;}
